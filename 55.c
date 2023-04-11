838868697ebb589e36ec122d292719b081aa38fc
Tioec ertuer <foxy070311@gmail.com>
2023-04-11 06:58:48 +0000

    sss

---
+++ 55.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+
+    int a = 3;
+    a = a + 6;
+    if(a>5) {
+        printf("%d\n", a);
+        printf("조건이 참일을 때, 실행 \n");
+	}else {
+        printf("%d\n", a);
+        printf("조건이 거짓일을 때, 실행 \n");
+}
