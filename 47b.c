c482a9d0857b6a3507d2a11414439e14dcacf3dc
Tioec ertuer <foxy070311@gmail.com>
2023-04-11 06:38:27 +0000

    dgs

---
+++ 47b.c
@@ -0,0 +1,12 @@
+int main () {
+
+    
+    int num1 = 591; //변수의 선언 및 초기화
+    int num2 = 3;
+    int sum = 0;
+
+    sum = num1 / num2;
+    printf("%d / %d = %d\n",num1, num2, sum);
+
+    return 0;
+}
