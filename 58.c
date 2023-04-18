802463317169d3fd20cb4831c9cbadd9d797c9f5
Tioec ertuer <foxy070311@gmail.com>
2023-04-18 07:29:23 +0000

    ajskasdkjlsfakj

---
+++ 58.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+
+    printf("%d %d\n",3==2, 3!=2);
+    printf("%d %d\n",3>2, 3!=2);
+    printf("%d\n", (3>2) && (3<=2));
+    printf("%d\n", (3>2) || (3<=2));
+    printf("%d\n", !(3>2));
+
+}
